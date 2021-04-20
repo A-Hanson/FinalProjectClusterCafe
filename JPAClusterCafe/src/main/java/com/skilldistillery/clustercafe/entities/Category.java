107042bd523fa9b06f7253327c85a42c355bcf38
Anna Hanson <>
2021-04-20 17:31:20 -0600

    category entity mapped to db

--- JPAClusterCafe/src/main/java/com/skilldistillery/clustercafe/entities/Category.java
+++ JPAClusterCafe/src/main/java/com/skilldistillery/clustercafe/entities/Category.java
@@ -1,5 +1,64 @@
 package com.skilldistillery.clustercafe.entities;
 
-public class Category {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+
+//	constructor
+	public Category() {}
+
+//	Methods
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
 }
