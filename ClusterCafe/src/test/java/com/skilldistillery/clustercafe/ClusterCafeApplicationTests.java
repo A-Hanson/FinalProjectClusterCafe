7f6d8cc885d6d60c81c0987ea707df42728c5251
bryceshearn <bryce_shearn@yahoo.com>
2021-04-20 13:05:36 -0400

    Create Boot/REST project

---
+++ ClusterCafe/src/test/java/com/skilldistillery/clustercafe/ClusterCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.clustercafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
