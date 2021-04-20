f0269058fc865bbcd595e1121f01bc47a67b4be5
ramonarmen96 <ramonarmen96@gmail.com>
2021-04-20 17:40:52 -0600

    Store Entity Stubbed out and passed initial test

--- JPAClusterCafe/src/main/java/com/skilldistillery/clustercafe/entities/Store.java
+++ JPAClusterCafe/src/main/java/com/skilldistillery/clustercafe/entities/Store.java
@@ -1,5 +1,148 @@
 package com.skilldistillery.clustercafe.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Store {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String address;
+	
+	private String city;
+	
+	private String state;
+	
+	@Column(name="postal_code")
+	private String postalCode;
+	
+	private float latitude;
+	
+	private float longitude;
+	
+	private String category;
+	
+	private boolean enabled;
+
+	public Store() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public float getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+
+	public float getLongtitude() {
+		return longitude;
+	}
+
+	public void setLongtitude(float longtitude) {
+		this.longitude = longtitude;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public String toString() {
+		return "Store [id=" + id + ", name=" + name + ", address=" + address + ", city=" + city + ", state=" + state
+				+ ", postalCode=" + postalCode + ", latitude=" + latitude + ", longitude=" + longitude + ", category="
+				+ category + ", enabled=" + enabled + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Store other = (Store) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
 }
