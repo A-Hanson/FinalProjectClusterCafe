198cbb2b4faf83243c9c3fea0a991767a2771d46
Anna Hanson <>
2021-04-21 09:54:47 -0600

    post_test to db mapping and testing

--- JPAClusterCafe/src/main/java/com/skilldistillery/clustercafe/entities/PostComment.java
+++ JPAClusterCafe/src/main/java/com/skilldistillery/clustercafe/entities/PostComment.java
@@ -1,5 +1,159 @@
 package com.skilldistillery.clustercafe.entities;
 
-public class PostComment {
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name="post_comment")
+public class PostComment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String content;
+	
+	@ManyToOne
+	@JoinColumn(name="post_id")
+	private Post post;
+	
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	private Boolean enabled = true;
+	
+	private Boolean flagged = false;
+	
+	@Column(name="created_at")
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	
+	@Column(name="updated_at")
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
+//	Constructor
+	public PostComment() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Boolean getFlagged() {
+		return flagged;
+	}
+
+	public void setFlagged(Boolean flagged) {
+		this.flagged = flagged;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("PostComment [id=");
+		builder.append(id);
+		builder.append(", content=");
+		builder.append(content);
+		builder.append(", post=");
+		builder.append(post);
+		builder.append(", user=");
+		builder.append(user);
+		builder.append(", enabled=");
+		builder.append(enabled);
+		builder.append(", flagged=");
+		builder.append(flagged);
+		builder.append(", createdAt=");
+		builder.append(createdAt);
+		builder.append(", updatedAt=");
+		builder.append(updatedAt);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PostComment other = (PostComment) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
 }
