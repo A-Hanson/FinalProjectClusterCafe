71c7fcb3ab4f3b1e3402595caced5e8fa0c05e84
ramonarmen96 <ramonarmen96@gmail.com>
2021-04-21 15:30:43 -0600

    404 page added

--- ngClusterCafe/src/app/app-routing.module.ts
+++ ngClusterCafe/src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: '**', component: NotFoundComponent },
+
 ];
 
 @NgModule({
