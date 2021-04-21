94ab8dba11479b97faf3856352ec6a2458fa2725
Anna Hanson <>
2021-04-21 12:14:37 -0600

    add registration form

--- ngClusterCafe/src/app/components/register/register.component.ts
+++ ngClusterCafe/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  newUser: User = new User();
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  register() {
+    this.authService.register(this.newUser).subscribe(
+      user => {
+        this.authService.login(user['username'], user['password']).subscribe(
+          success => {
+            console.log("User Logged In!!")
+            this.router.navigateByUrl('/#######');
+          },
+          fail => {
+            console.log("User unable to login: " + fail);
+            this.router.navigateByUrl('/notFound');
+          }
+        );
+        this.newUser = new User();
+      },
+      fail => {
+        console.log("Unable to register User: " + fail);
+        this.router.navigateByUrl('/notFound');
+      }
+    );
   }
 
 }
