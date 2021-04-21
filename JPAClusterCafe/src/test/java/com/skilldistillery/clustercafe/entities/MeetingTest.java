5cec6efc12ad7dbd52533e9b6271b8b9240cd4f3
ramonarmen96 <ramonarmen96@gmail.com>
2021-04-21 10:43:12 -0600

    Meeting Entity Stubbed and tested Group_User join table mapped

---
+++ JPAClusterCafe/src/test/java/com/skilldistillery/clustercafe/entities/MeetingTest.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.clustercafe.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MeetingTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Meeting meeting;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("ClusterCafePU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		meeting = em.find(Meeting.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		meeting = null;
+	}
+
+	@Test
+	@DisplayName("Test Meeting mapping")
+	void test_1() {
+		assertNotNull(meeting);
+		assertEquals("Jogging", meeting.getName());
+	}
+
+
+}
