1d124a75f1081c5b83331f337cb9ccbdf090b1c2
Anna Hanson <>
2021-04-23 13:32:53 -0600

    add bootstrap icons

--- ngClusterCafe/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ ngClusterCafe/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Category } from 'src/app/models/category';
+import { Post } from 'src/app/models/post';
+import { PostComment } from 'src/app/models/postComment';
+import { User } from 'src/app/models/user';
+import { CategoryService } from 'src/app/services/category.service';
+import { PostService } from 'src/app/services/post.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,10 +14,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
+  posts: Post[] = [];
+  selectedPost: Post = null;
+  categories: Category[] = [];
+  newPostCategory: Category = null;
+  postComments: PostComment[] = [];
+  selectedComment: PostComment = null;
+  currentUser: User = null;
 
-  constructor() { }
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private categoryService: CategoryService,
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  loadInitial() {
+
   }
 
 }
