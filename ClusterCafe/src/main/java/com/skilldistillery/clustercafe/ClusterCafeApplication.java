d30f2bf121265cf1fb29f0517f99d95fc9f64586
ramonarmen96 <ramonarmen96@gmail.com>
2021-04-26 10:49:26 -0600

    EC2 Deployment commit

--- ClusterCafe/src/main/java/com/skilldistillery/clustercafe/ClusterCafeApplication.java
+++ ClusterCafe/src/main/java/com/skilldistillery/clustercafe/ClusterCafeApplication.java
@@ -2,12 +2,19 @@ package com.skilldistillery.clustercafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class ClusterCafeApplication {
+public class ClusterCafeApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ClusterCafeApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClusterCafeApplication.class, args);
