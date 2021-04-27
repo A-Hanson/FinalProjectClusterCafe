c750bcf6223d20b33fc0cd799b757d076e1053fd
Anna Hanson <>
2021-04-27 10:18:14 -0600

    store backend api created

---
+++ ClusterCafe/src/main/java/com/skilldistillery/clustercafe/repositories/StoreRepository.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.clustercafe.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.clustercafe.entities.Store;
+
+public interface StoreRepository extends JpaRepository<Store, Integer>{
+	List<Store> findByEnabledTrue();
+}
